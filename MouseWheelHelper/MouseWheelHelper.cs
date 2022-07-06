2d2397798b2369c4eb42f67c8d3ac9be7f6dc9c1
John Allison <john@jallison.co.uk>
2022-07-06 15:00:04 +0100

    basic form and mouse wheel helper

---
+++ MouseWheelHelper/MouseWheelHelper.cs
@@ -0,0 +1,52 @@
+﻿using Gma.System.MouseKeyHook;
+
+namespace MouseWheelHelperLib
+{
+    public class MouseWheelHelper
+    {
+        private IKeyboardMouseEvents? _hook;
+        private bool _disposed = false;
+
+        public MouseWheelHelper()
+        {
+            Subscribe();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                Unsubscribe();
+            }
+            _disposed = true;
+        }
+
+        private void Subscribe()
+        {
+            if (_hook is not null)
+            {
+                _hook = Hook.GlobalEvents();
+                _hook.MouseWheelExt += Scroll;
+            }
+        }
+
+        private void Scroll(object sender, MouseEventExtArgs e)
+        {
+
+        }
+
+        private void Unsubscribe()
+        {
+            if (_hook is not null)
+            {
+                _hook.MouseWheelExt -= Scroll;
+                _hook.Dispose();
+            }
+        }
+    }
+}
---
+++ SpotifyVolumeControllerV4/Menu.cs
@@ -0,0 +1,14 @@
+using MouseWheelHelperLib;
+
+namespace SpotifyVolumeControllerV4
+{
+    public partial class Menu : Form
+    {
+        private MouseWheelHelper mouseWheelHelper = new();
+
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
